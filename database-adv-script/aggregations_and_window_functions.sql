16d077d76852825ea30baae4bbd40b606c26bf29
Moment49 <ibenachoelvis49@gmail.com>
2025-07-04 04:00:54 +0100

    feat:worked with window functions (RANK)

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,19 @@
+USE alx_airbnb;
+
+-- Write a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+SELECT u.user_id, u.first_name, u.last_name, COUNT(b.booking_id) as TotalBookings
+FROM booking b
+INNER JOIN users u
+ON (b.user_id = u.user_id)
+GROUP BY user_id;
+
+-- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
+SELECT * from property;
+SELECT * from booking;
+
+SELECT property_id, name FROM property;
+
+SELECT property_id, COUNT(booking_id) as totalBooking,
+  RANK() OVER (ORDER BY COUNT(booking_id) DESC) 
+from booking
+GROUP BY property_id;
